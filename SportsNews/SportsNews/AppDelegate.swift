c53a71d6a94b1324ef2ae64369ae5714f94c137b
James Militante <james.militante@gmail.com>
2020-02-28 20:27:03 -0500

    added launch screen

--- SportsNews/SportsNews/AppDelegate.swift
+++ SportsNews/SportsNews/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // launch screen delay
+        Thread.sleep(forTimeInterval: 2.0)
         return true
     }
 
--- SportsNews/SportsNews/Models/TweetsManager.swift
+++ SportsNews/SportsNews/Models/TweetsManager.swift
@@ -5,29 +5,7 @@
 //  Created by James Militante on 2019-10-03.
 //  Copyright © 2019 James Militante. All rights reserved.
 //
-
-//    func getPost(input: String) {
-//        if let url = URL(string: "https://www.reddit.com/r/\(input)/search.json?q=url%3Atwitter&restrict_sr=on&sort=new&t=week"){
-//            let session = URLSession(configuration: .default)
-//            let task = session.dataTask(with: url) { (data, response, error) in
-//                if error == nil {
-//                    let decoder = JSONDecoder()
-//                    if let safeData = data {
-//                        do{
-//                            let results = try decoder.decode(Posts.self, from: safeData)
-//                            DispatchQueue.main.async {
-//                                self.posts = results.data.children
-//                            }
-//                        } catch {
-//                            print(error)
-//                        }
-//                    }
-//                }
-//            }
-//            task.resume()
-//        }
-//    }
-
+ 
 import Foundation
 
 class TweetsManager: ObservableObject{
